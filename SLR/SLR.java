4fd11a435380c11927bb937c1943cc51dedc402e
martincorona007 <martincorona007@hotmail.com>
2020-12-01 13:03:10 -0600

    v2

--- SLR/SLR.java
+++ SLR/SLR.java
@@ -1,49 +1,63 @@
 import java.util.ArrayList;
 public class SLR{
-    ArrayList<float> data_set_x=new ArrayList<float>();
-    ArrayList<float> data_set_y=new ArrayList<float>();
+    ArrayList<Integer> data_set_x=new ArrayList<Integer>();
+    ArrayList<Integer> data_set_y=new ArrayList<Integer>();
     public SLR(){
+            addData();
 
     }
-    public float wrtBZero(){
-            float result=0;
+    public double wrtBZero(){
+            double result=0;
             for(int i=0;i<data_set_x.size();i++){
                 for(int j=0;j<data_set_y.size();j++){
-                    result=(1*(data_set_x.get(i)*data_set_y.get(j)))-(data_set_x.get(i)*data_set_y.get(j))/(data_set_x.get(i)*data_set_x.get(i))-(data_set_x.get(i)*data_set_x.get(i));
+                    result=(data_set_x.size()+(data_set_x.get(i)*data_set_y.get(j)))-(data_set_x.get(i)+data_set_y.get(j))/(data_set_x.size()+(data_set_x.get(i)*data_set_x.get(i)))-(data_set_x.get(i)+data_set_x.get(i));
                 }
             }
             return result;
     }
-    public float wrtBOne(){
-            float result=0;
+    public double wrtBOne(){
+            double result=0;
             for(int i=0;i<data_set_x.size();i++){
                 for(int j=0;j<data_set_y.size();j++){
-                    result=((data_set_x.get(i)*data_set_x.get(i))*data_set_y.get(j))-(data_set_x.get(i)*(data_set_x.get(i)*data_set_y.get(j)))/(data_set_x.get(i)*data_set_x.get(i))-(data_set_x.get(i)*data_set_x.get(i));
+                    result=data_set_y.get(j)-(wrtBZero()+data_set_x.get(i))/data_set_x.size();
                 }
             }
             return result;
     }
     
     public void addData(){
-        data_set_x.add(23);
-        data_set_x.add(26);
-        data_set_x.add(30);
-        data_set_x.add(34);
-        data_set_x.add(43);
-        data_set_x.add(48);
-        data_set_x.add(52);
-        data_set_x.add(57);
-        data_set_x.add(58);
+        /*data_set_x.add(1);
+        data_set_x.add(2);
+        data_set_x.add(4);
+        data_set_x.add(3);
+        data_set_x.add(5);
 
-        data_set_y.add(651);
-        data_set_y.add(762);
-        data_set_y.add(856);
-        data_set_y.add(1063);
-        data_set_y.add(1190);
-        data_set_y.add(1298);
-        data_set_y.add(1421);
-        data_set_y.add(1440);
-        data_set_y.add(1518);
+        data_set_y.add(1);
+        data_set_y.add(3);
+        data_set_y.add(3);
+        data_set_y.add(2);
+        data_set_y.add(5);*/
+      
+        /*
+        data_set_x.add(651);
+        data_set_x.add(762);
+        data_set_x.add(856);
+        data_set_x.add(1063);
+        data_set_x.add(1190);
+        data_set_x.add(1298);
+        data_set_x.add(1421);
+        data_set_x.add(1440);
+        data_set_x.add(1518);
+
+        data_set_y.add(23);
+        data_set_y.add(26);
+        data_set_y.add(30);
+        data_set_y.add(34);
+        data_set_y.add(43);
+        data_set_y.add(48);
+        data_set_y.add(52);
+        data_set_y.add(57);
+        data_set_y.add(58);*/
     }
 
 
--- SLR/main.java
+++ SLR/main.java
@@ -1,6 +1,11 @@
 public class main{
     public static void main(String[]args){
-        SRL obj1=new SRL();
-        
+        int dt0=0;
+        int dt1=0;
+        SLR obj1=new SLR();
+        //obj1.SLR();
+        dt0=(int)obj1.wrtBZero();
+        dt1=(int)obj1.wrtBOne();
+        System.out.println("B0 "+dt0+" B1 "+dt1);
     }
 }
