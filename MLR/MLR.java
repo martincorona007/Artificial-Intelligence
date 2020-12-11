84f1479b35dec746be65df7b3858938f0eb62f84
martincorona007 <martincorona007@hotmail.com>
2020-12-11 11:28:15 -0600

    v2 checking errors with B0 and B1

--- MLR/MLR.java
+++ MLR/MLR.java
@@ -13,7 +13,7 @@ public class MLR{
         double result=0;
         double numerator=0;
         double denominator=0;
-        numerator=(((sumYi()*sumX2i1()*sumX2i2())+(sumXi1()*sumXi1Xi2()*sumXi2Yi())+(sumXi2()*sumXi1Yi()*sumXi1Xi2()))-((sumXi2Yi()*sumX2i1()*sumXi2())-(sumXi1Xi2()*sumXi1Xi2()*sumYi())-(sumX2i2()*sumXi1Yi()*sumXi1())));
+        numerator=((sumYi()*sumX2i1()*sumX2i2())-(sumXi1()*sumXi1Xi2()*sumXi2Yi())-(sumXi2()*sumXi1Yi()*sumXi1Xi2())-(sumXi2Yi()*sumX2i1()*sumXi2())-(sumXi1Xi2()*sumXi1Xi2()*sumYi())-(sumX2i2()*sumXi1Yi()*sumXi1()));
         denominator=DS();
         result=numerator/denominator;
         return result;
@@ -23,7 +23,7 @@ public class MLR{
         double result=0;
         double numerator=0;
         double denominator=0;
-        numerator=(((ds_Xi1.size()*sumXi1Yi()*sumX2i2())+(sumYi()*sumXi1Xi2()*sumXi2())+(sumXi2()*sumXi1()*sumXi2Yi()))-((sumXi2()*sumXi1Yi()*sumXi2())-(sumXi2Yi()*sumXi1Xi2()*ds_Xi1.size())-(sumX2i2()*sumXi1()*sumYi())));
+        numerator=((ds_Xi1.size()*sumXi1Yi()*sumX2i2())+(sumYi()*sumXi1Xi2()*sumXi2())+(sumXi2()*sumXi1()*sumXi2Yi())-(sumXi2()*sumXi1Yi()*sumXi2())-(sumXi2Yi()*sumXi1Xi2()*ds_Xi1.size())-(sumX2i2()*sumXi1()*sumYi()));
         denominator=DS();
         result=numerator/denominator;
         return result;
@@ -33,7 +33,7 @@ public class MLR{
     }*/
     //determinat of the system
     public double DS(){
-        return (((ds_Xi1.size()*sumX2i1()*sumXi1())+(sumXi1()*sumXi1Xi2()*sumXi2())+(sumXi2()*sumXi1()*sumXi1Xi2()))-((sumXi2()*sumX2i1()*sumXi2())-(sumXi1Xi2()*sumXi1Xi2()*ds_Xi1.size())-(sumX2i2()*sumXi1()*sumXi1())));
+        return ((ds_Xi1.size()*sumX2i1()*sumXi1())-(sumXi1()*sumXi1Xi2()*sumXi2())-(sumXi2()*sumXi1()*sumXi1Xi2())-(sumXi2()*sumX2i1()*sumXi2())-(sumXi1Xi2()*sumXi1Xi2()*ds_Xi1.size())-(sumX2i2()*sumXi1()*sumXi1()));
     }
     public double sumX2i1(){
         double val=0;
