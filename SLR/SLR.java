ee2bf8d35e584ee6cd6b7e49ecd396de6c14b619
martincorona007 <martincorona007@hotmail.com>
2020-12-12 18:05:23 -0600

    v12 adding GA base

--- SLR/SLR.java
+++ SLR/SLR.java
@@ -61,7 +61,7 @@ public class SLR{
         return val;
     }
     public void addData(){
-        ds_X.add(1);
+        /*ds_X.add(1);
         ds_X.add(2);
         ds_X.add(4);
         ds_X.add(3);
@@ -71,7 +71,7 @@ public class SLR{
         ds_Y.add(3);
         ds_Y.add(3);
         ds_Y.add(2);
-        ds_Y.add(5);
+        ds_Y.add(5);*/
         
         /*ds_X.add(2);
         ds_X.add(4);
@@ -108,7 +108,7 @@ public class SLR{
         ds_Y.add(3);
         ds_Y.add(6);
         */
-        /*
+        
         ds_X.add(651);
         ds_X.add(762);
         ds_X.add(856);
@@ -128,14 +128,14 @@ public class SLR{
         ds_Y.add(52);
         ds_Y.add(57);
         ds_Y.add(58);
-        */
+        
        
     }
 
 public static void main(String[]args){
         double dt0=0;
         double dt1=0;
-
+        double pre=0;
         SLR obj1=new SLR();
         System.out.println("x "+obj1.sumXi());
         System.out.println("y "+obj1.sumYi());
@@ -145,6 +145,8 @@ public static void main(String[]args){
         dt0=obj1.wrtBZero();
         dt1=obj1.wrtBOne();
         System.out.println("B0 "+dt0+" B1 "+dt1);
+        pre=dt0+(dt1*1518);
+        System.out.println("y "+pre);
 }
 
 }
